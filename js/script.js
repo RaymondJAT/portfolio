731374f72e01861e68b8c28e8da5b3a8bf008d1c
Raymond Justine Talag <rjat081700@gmail.com>
2024-08-06 23:10:30 +0800

    script revised, css revised

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ window.addEventListener("scroll", function () {
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
-menu.onClick = () => {
+menu.onclick = () => {
   menu.classList.toggle("bx-x");
   navbar.classList.toggle("open");
 };
