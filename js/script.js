75b4b2e1f7bfa974e2f095dcc9a09f37b6d73b75
Raymond Justine Talag <rjat081700@gmail.com>
2024-08-08 14:21:27 +0800

    CSS, script Revised

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const header = document.querySelector("header");
 
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", window.scrollY > 60);
 });
 
